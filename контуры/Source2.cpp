170b117673560f3395ed914f0320fd03a2d9aea8
MustafaAlibekov <89997083+MustafaAlibekov@users.noreply.github.com>
2021-09-28 09:51:31 +0300

    Add files via upload

---
+++ контуры/Source2.cpp
@@ -0,0 +1,33 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <string>
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <windows.h>
+using namespace cv;
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	char filename[80]; //Luffy_Anime_Post_Timeskip_Infobox.jpg
+	cout << "Введите имя файла, в который хотите внести изменения и нажмите Enter" << endl;
+	cin.getline(filename, 80);
+	cout << filename << endl;
+	Mat img = imread(filename, 1);
+	namedWindow("Исходное изображение" , WINDOW_AUTOSIZE);
+	imshow("Исходное изображение", img);
+	Mat src_gray; Mat canny_output;
+		cvtColor(img, src_gray, COLOR_RGB2GRAY);
+	blur(src_gray, src_gray, Size(3, 3));
+	double otsu_thresh_val = threshold(src_gray, img, 0, 255, THRESH_BINARY | THRESH_OTSU);
+	double high_thresh_val = otsu_thresh_val, lower_thresh_val=otsu_thresh_val * 0.5;
+	cout << otsu_thresh_val;
+	Canny(src_gray, canny_output, lower_thresh_val, high_thresh_val, 3);
+	namedWindow("Серое изображение", WINDOW_AUTOSIZE);
+	imshow("Серое изображение", canny_output);
+	imwrite("canny_output.jpg", canny_output);
+	waitKey(0);
+	return 0;
+}
